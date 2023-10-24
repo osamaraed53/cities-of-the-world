492fa7836e296535007197982e2eb74d981ce3dc
osamaraed53 <osamaraed53@gmail.com>
2023-10-24 08:53:15 +0300

    new

---
+++ my-project/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Nav';
+import Hero from './Hero';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
